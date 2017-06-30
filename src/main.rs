871f74c5aee195aa11b88dcd84570fa76b52d20c
Panagiotis Koutsourakis <kutsurak@monetdbsolutions.com>
2017-06-30 13:15:13 +0200

    Add a main file for simple testing

---
+++ src/main.rs
@@ -0,0 +1,46 @@
+extern crate rumonetdb;
+
+use rumonetdb::mapi::*;
+
+fn main() {
+
+    println!("Connecting to local merovignian");
+    let dparams = MapiConnectionParams::new("marvin");
+
+    let mut c1 = MapiConnection::connect(dparams).unwrap();
+    let res1 = c1.cmd("sSELECT * from _tables;").unwrap();
+    println!("Response = {}", res1);
+    // let ds = c1.get_bytes().unwrap();
+
+    // println!("Daemon sent: {:?}", ds);
+
+    // println!();
+    // println!("Connecting to local mserver...");
+    // let mut sparams = MapiConnectionParams::new("foo");
+    // sparams.port = Some(30000);
+
+    // let mut c2 = MapiConnection::connect(sparams).unwrap();
+    // println!("connected");
+    // let res2 = c2.cmd("sSELECT * from _tables limit 3;").unwrap();
+    // println!("Response = {}", res2);
+    // let ss = c2.get_bytes().unwrap();
+
+    // println!("Server sent: {:?}", ss);
+
+    // println!();
+    // println!("Connecting to merovingian through unix socket");
+    // let mut uparams = MapiConnectionParams::new("marvin");
+    // uparams.hostname = Some(String::from("/tmp"));
+
+    // let mut c3 = MapiConnection::connect(uparams).unwrap();
+    // let us = c3.get_bytes().unwrap();
+
+    // println!("Socket sent: {:?}", us);
+
+    // println!();
+    // println!("Connecting to remote merovingian");
+    // let mut rparams = MapiConnectionParams::new("SF-10");
+    // rparams.hostname = Some(String::from("marvinaws"));
+
+    // let mut c3 = MapiConnection::connect(rparams).unwrap();
+}
