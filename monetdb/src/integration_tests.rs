17cf81a575a1aaaf6cab3e17e1f895975d179fcd
MitchellWeg <mitchell.w@live.nl>
2022-07-10 11:33:41 +0200

    implement more integration tests

--- monetdb/src/integration_tests.rs
+++ monetdb/src/integration_tests.rs
@@ -12,4 +12,43 @@ mod tests {
             }
         }
     }
+
+    #[test]
+    fn we_can_create_a_table() {
+        let mut monetdb = crate::Connection::connect("mapi://localhost:50000/demo").unwrap();
+        let result = monetdb.execute("CREATE TABLE IF NOT EXISTS foo (i int)");
+        match result {
+            Ok(_) => assert!(true),
+            Err(e) => {
+                eprintln!("{}", e);
+                assert!(false);
+            }
+        }
+    }
+
+    #[test]
+    fn we_can_insert_into_a_table() {
+        let mut monetdb = crate::Connection::connect("mapi://localhost:50000/demo").unwrap();
+        let result = monetdb.execute("INSERT INTO foo VALUES (1), (2)");
+        match result {
+            Ok(_) => assert!(true),
+            Err(e) => {
+                eprintln!("{}", e);
+                assert!(false);
+            }
+        }
+    }
+
+    #[test]
+    fn we_can_select_from_a_table() {
+        let mut monetdb = crate::Connection::connect("mapi://localhost:50000/demo").unwrap();
+        let result = monetdb.execute("SELECT * FROM foo");
+        match result {
+            Ok(_) => assert!(true),
+            Err(e) => {
+                eprintln!("{}", e);
+                assert!(false);
+            }
+        }
+    }
 }
