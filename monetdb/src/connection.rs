7ea95a107e023f4f2128ffbc9114cd48eb7e24ed
MitchellWeg <mitchell.w@live.nl>
2022-08-08 12:13:58 +0200

    small query fix

--- monetdb/src/connection.rs
+++ monetdb/src/connection.rs
@@ -61,17 +61,25 @@ impl Connection {
         Ok(insertions)
     }
 
-    // TODO: this should take parameters
     // TODO: maybe return a T that represents the type?
-    pub fn query(&mut self, query: &str) -> Result<Vec<String>> {
-        let command = String::from("s") + query + "\n;";
+    pub fn query(&mut self, query: &str, params: Vec<monetizer::SQLParameter>) -> Result<Vec<String>> {
+        let escaped_query = monetizer::apply_parameters(query, params);
+        let command = String::from("s") + &escaped_query + "\n;";
         let resp = self.connection.cmd(&command[..])?;
 
-        let output = resp
-                .lines()
-                .skip(5)
-                .map(|x| x.to_string().replace(&['\t', '[', ']', ' '], ""))
-                .collect::<Vec<String>>();
+        let response_lines = resp.lines();
+
+        let response_header = response_lines.skip(2);
+        let response_body = response_header.skip(3);
+
+        let mut output: Vec<String> = Vec::new();
+
+        for line in response_body {
+            let sanitized = line.replace(&['\t', '[', ']', ' '], "");
+            let mut splitted = sanitized.split(',').map(|x| x.to_string()).collect::<Vec<String>>();
+
+            output.append(&mut splitted);
+        }
 
         Ok(output)
     }
--- monetdb/src/integration_tests.rs
+++ monetdb/src/integration_tests.rs
@@ -16,21 +16,35 @@ mod tests {
     fn create_insert_select_test() -> Result<(), MonetDBError> {
         let mut monetdb = Connection::connect("mapi://localhost:50000/demo")?;
         monetdb.execute("DROP TABLE IF EXISTS foo", vec![])?;
-        monetdb.execute("CREATE TABLE foo (i int)", vec![])?;
-        let result = monetdb.execute("INSERT INTO foo VALUES (1), (2)", vec![])?;
+        monetdb.execute("CREATE TABLE foo (i int, x int)", vec![])?;
+        let result = monetdb.execute("INSERT INTO foo VALUES (1, 2), (2, 3)", vec![])?;
 
         assert_eq!(result, 2);
         let result = monetdb.execute(
-            "INSERT INTO foo VALUES ({}), ({})",
-            vec![to_sqlparameter(1), to_sqlparameter(2)],
+            "INSERT INTO foo VALUES ({}, {}), ({}, {})",
+            vec![to_sqlparameter(1), to_sqlparameter(2), to_sqlparameter(2), to_sqlparameter(4)],
         )?;
-        assert_eq!(result, 2);
-        let result = monetdb.execute("SELECT * FROM foo", vec![])?;
-        assert_eq!(result, 0); // ! Not correct. The execute function needs work.
 
-        let result = monetdb.query("SELECT * FROM foo")?;
-        assert_eq!(result.len(), 4); // ! Not correct. The execute function needs work.
-        assert_eq!(result, vec!["1", "2", "1", "2"]); // ! Not correct. The execute function needs work.
+        assert_eq!(result, 2);
+
+        Ok(())
+    }
+
+    #[test]
+    fn query_row_test() -> Result<(), MonetDBError> {
+        let mut monetdb = Connection::connect("mapi://localhost:50000/demo")?;
+        monetdb.execute("DROP TABLE IF EXISTS foo2", vec![])?;
+        monetdb.execute("CREATE TABLE foo2 (i int, x int)", vec![])?;
+        let result = monetdb.execute("INSERT INTO foo2 VALUES (1, 2), (2, 3)", vec![])?;
+
+        let result = monetdb.query(
+            "SELECT * FROM foo2 WHERE i = ({})",
+            vec![to_sqlparameter(2)],
+        )?;
+
+        assert_eq!(result.len(), 2); 
+        assert_eq!(result, vec!["2", "3"]); 
+    
 
         Ok(())
     }
