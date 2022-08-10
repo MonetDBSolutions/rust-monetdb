a724c906ee7bddacc19e4c65944c3925d796a8d4
MitchellWeg <mitchell.w@live.nl>
2022-08-10 15:34:37 +0200

    add unit tests for MonetType

--- monetdb/src/row.rs
+++ monetdb/src/row.rs
@@ -22,3 +22,44 @@ pub struct Row {
     pub value: Vec<Option<MonetType>>
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn ints_are_parsed_correctly() {
+        let input = MonetType::parse(&String::from("int"), "1").unwrap();
+        let input1 = MonetType::parse(&String::from("int"), "100").unwrap();
+        let input2 = MonetType::parse(&String::from("int"), "999").unwrap();
+
+        assert_eq!(input, MonetType::Int(1));
+        assert_eq!(input1, MonetType::Int(100));
+        assert_eq!(input2, MonetType::Int(999));
+    }
+
+    #[test]
+    fn floats_are_parsed_correctly() {
+        let input = MonetType::parse(&String::from("double"), "1.0").unwrap();
+        let input1 = MonetType::parse(&String::from("double"), "100.9").unwrap();
+        let input2 = MonetType::parse(&String::from("double"), "999.9").unwrap();
+
+        assert_eq!(input, MonetType::Double(1.0));
+        assert_eq!(input1, MonetType::Double(100.9));
+        assert_eq!(input2, MonetType::Double(999.9));
+    }
+
+    #[test]
+    fn clobs_are_parsed_correctly() {
+        let input = MonetType::parse(&String::from("clob"), "foo").unwrap();
+        let input1 = MonetType::parse(&String::from("clob"), "bar").unwrap();
+        let input2 = MonetType::parse(&String::from("clob"), "999.9").unwrap();
+        let input3 = MonetType::parse(&String::from("clob"), "foo bar with a lot of spaces").unwrap();
+        let input4 = MonetType::parse(&String::from("clob"), "'''foo bar with a lot of backticks'''").unwrap();
+
+        assert_eq!(input, MonetType::MapiString(String::from("foo")));
+        assert_eq!(input1, MonetType::MapiString(String::from("bar")));
+        assert_eq!(input2, MonetType::MapiString(String::from("999.9")));
+        assert_eq!(input3, MonetType::MapiString(String::from("foo bar with a lot of spaces")));
+        assert_eq!(input4, MonetType::MapiString(String::from("'''foo bar with a lot of backticks'''")));
+    }
+}
