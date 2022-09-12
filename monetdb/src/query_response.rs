a2c2d93630b0055bb009cecd0c3ced449de34409
MitchellWeg <mitchell.w@live.nl>
2022-09-12 12:58:27 +0200

    add the ability to parse the metadata header

--- monetdb/src/query_response.rs
+++ monetdb/src/query_response.rs
@@ -18,6 +18,7 @@ pub struct QueryResponse {
     pub result_id: i32,
     pub number_of_rows: i32,
     pub column_count: i32,
+    pub number_of_rows_in_message: i32,
     pub query_id: i32,
     pub query_time: i32,
     pub mal_optimizer_time: i32,
@@ -27,20 +28,29 @@ pub struct QueryResponse {
 
 impl QueryResponse {
     pub fn new(resp: String) -> Result<QueryResponse> {
+        let response_lines: Vec<&str> = resp.lines().collect();
+        let metadata_header = match response_lines.get(0) {
+            Some(s) => s,
+            None => return Err(MonetDBError::UnimplementedError(String::from("Received no metadata")))
+        };
+
+        let metadata_header = QueryResponse::parse_metadata_header(&metadata_header);
+
         let result_rows = match QueryResponse::parse_response_output(resp) {
             Ok(s) => s,
             Err(e) => return Err(e)
         };
 
         Ok(QueryResponse {
-            response_type: ResponseType::Data,
-            result_id: 0,
-            number_of_rows: 0,
-            column_count: 0,
-            query_id: 0,
-            query_time: 0,
-            mal_optimizer_time: 0,
-            sql_optimizer_time: 0,
+            response_type: metadata_header.0,
+            result_id: metadata_header.1,
+            number_of_rows: metadata_header.2,
+            column_count: metadata_header.3,
+            number_of_rows_in_message: metadata_header.4,
+            query_id: metadata_header.5,
+            query_time: metadata_header.6,
+            mal_optimizer_time: metadata_header.7,
+            sql_optimizer_time: metadata_header.8,
             result: result_rows
         })
     }
@@ -77,6 +87,29 @@ impl QueryResponse {
             }
 
             Ok(output)
+        }
+
+        #[inline]
+        fn parse_metadata_header(input: &str) -> (ResponseType, i32, i32, i32, i32, i32, i32, i32, i32) {
+            let header = input.to_string(); 
+            let splitted: Vec<&str> = header.split(' ').collect();
+
+            let response_type: ResponseType = match splitted[0] {
+                "&1" => ResponseType::Data,
+                _ => ResponseType::Data
+            };
+
+            (
+                response_type,
+                splitted[1].parse().unwrap(),
+                splitted[2].parse().unwrap(),
+                splitted[3].parse().unwrap(),
+                splitted[4].parse().unwrap(),
+                splitted[5].parse().unwrap(),
+                splitted[6].parse().unwrap(),
+                splitted[7].parse().unwrap(),
+                splitted[8].parse().unwrap(),
+            )
         }
 
         #[inline]
@@ -127,12 +160,13 @@ mod tests {
             let desired = QueryResponse {
                  response_type: ResponseType::Data,
                  result_id: 0,
-                 number_of_rows: 0,
-                 column_count: 0,
-                 query_id: 0,
-                 query_time: 0,
-                 mal_optimizer_time: 0,
-                 sql_optimizer_time: 0,
+                 number_of_rows: 2,
+                 column_count: 2,
+                 number_of_rows_in_message: 2,
+                 query_id: 1443,
+                 query_time: 1918,
+                 mal_optimizer_time: 479,
+                 sql_optimizer_time: 178,
                  result: vec![
                     Row { value: vec![Int(1), MapiString("foo".to_string())] },
                     Row { value: vec![Int(2), MapiString("bar".to_string())] },
@@ -158,12 +192,13 @@ mod tests {
             let desired = QueryResponse {
                  response_type: ResponseType::Data,
                  result_id: 0,
-                 number_of_rows: 0,
-                 column_count: 0,
-                 query_id: 0,
-                 query_time: 0,
-                 mal_optimizer_time: 0,
-                 sql_optimizer_time: 0,
+                 number_of_rows: 2,
+                 column_count: 1,
+                 number_of_rows_in_message: 2,
+                 query_id: 1496,
+                 query_time: 896,
+                 mal_optimizer_time: 1242,
+                 sql_optimizer_time: 55,
                  result: vec![
                     Row { value: vec![MapiString("And He said: \\\"Let there be Light!\\\"".to_string())] },
                     Row { value: vec![MapiString("Very hard string: [%]".to_string())] },
